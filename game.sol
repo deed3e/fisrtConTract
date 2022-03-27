7f068a9f19f1a2ad01c790880acee849f5880b11
tienhung164 <hungvt164@gmail.com>
2022-03-27 12:14:10 +0700

    done game

--- game.sol
+++ game.sol
@@ -1,1 +1,26 @@
-game.sol
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract game{
+    uint public countPlayer =0;
+    mapping(address => Player) public  players;
+    enum level{newbie,medium,highgt} 
+
+    struct Player {
+     	address addr;
+     	uint amount;
+     	string name;
+     	uint age;
+        level lv; 
+     	
+     }
+
+
+    function addPlayer(uint amount, string memory name ,uint age) public {
+                players[msg.sender]=Player(msg.sender,amount,name,age,level.newbie);
+                countPlayer +=1;
+
+     }
+     
+
+}
